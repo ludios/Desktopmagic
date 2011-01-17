fc5930f2dee1a811a4db48bee3dda8267996b5fb
ivank <ivank@ludios.net>
2011-01-17 02:57:10 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+import desktopmagic
+
+setup(
+	name='desktopmagic',
+	version=desktopmagic.__version__,
+	description="Python utilities useful in a desktop environment.",
+	packages=['desktopmagic', 'desktopmagic.test'],
+)
