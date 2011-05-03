d810170b9ecab2b3e11369cd15a9736645e24f2f
ivank <ivan@ludios.org>
2011-05-03 03:25:14 +0000

    Add MANIFEST.in; change program name: desktopmagic -> Desktopmagic; fix setup.py for uploading to Pypi

--- setup.py
+++ setup.py
@@ -5,9 +5,20 @@ from distutils.core import setup
 import desktopmagic
 
 setup(
-	name='desktopmagic',
+	name='Desktopmagic',
 	version=desktopmagic.__version__,
-	description="Python utilities useful in a desktop environment.",
+	description="Robust multi-monitor screenshot grabber (Windows-only right now)",
+	url="https://github.com/ludios/Desktopmagic",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 4 - Beta',
+		'Environment :: Win32 (MS Windows)',
+		'Operating System :: Microsoft :: Windows',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	packages=['desktopmagic', 'desktopmagic.scripts', 'desktopmagic.test'],
 	scripts=['bin/screengrab_torture_test'],
 )
